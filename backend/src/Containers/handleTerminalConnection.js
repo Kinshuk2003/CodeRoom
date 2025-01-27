452bf5bc24d222f26f89a21227823eefdb8e75e8
Kinshuk2020 <kinshukgoyal2003@gmail.com>
2025-01-27 22:00:28 +0530

    adding running of default commands in the terminal

--- backend/src/Containers/handleTerminalConnection.js
+++ backend/src/Containers/handleTerminalConnection.js
@@ -1,45 +1,73 @@
-export const handleTerminalConnection = (container, ws) => {
-    console.log(" Terminal Container id", container.id);
-    
+export const handleTerminalConnection = (container, ws, projectId) => {
+    console.log("Terminal Container id", container.id);
+
+    // Execute default commands before allowing user to start giving commands
     container.exec({
-        Cmd: ["/bin/bash"],
-        AttachStdin: true,
+        Cmd: ['sh', '-c', `cd $(ls -d */ | head -n 1) && npm install`],
         AttachStdout: true,
         AttachStderr: true,
         Tty: true,
         User: "sandbox",
     }, (err, exec) => {
-        if(err) {
+        if (err) {
             console.log("Error while creating exec", err);
             return;
         }
 
-        exec.start({
-                hijack: true,
-            }, (err, stream) => {
-                if(err) {
-                    console.log("Error while starting exec", err);
-                    return;
-                }
+        exec.start({ hijack: true, stdin: true }, (err, stream) => {
+            if (err) {
+                console.log("Error while starting exec", err);
+                return;
+            }
 
-                // Step 1: Stream processing
-                processStreamOutput(stream, ws);
-                // Step 2: Stream writing
+            // Show logs of the default commands to the user
+            stream.on('data', (data) => {
+                ws.send(data.toString());
+            });
 
-                ws.on("message", (data) => {
-                    if(data === "getPort") {
-                        container.inspect((err, data) => {
-                            const port = data.NetworkSettings;
-                            console.log(port);
-                        })
+            stream.on('end', () => {
+                console.log("npm install executed");
+
+                // Now connect the terminal from the frontend to the Docker container
+                container.exec({
+                    Cmd: ["/bin/bash"],
+                    AttachStdin: true,
+                    AttachStdout: true,
+                    AttachStderr: true,
+                    Tty: true,
+                    User: "sandbox",
+                }, (err, exec) => {
+                    if (err) {
+                        console.log("Error while creating exec", err);
                         return;
                     }
-                    stream.write(data);
-                })
-            }
-        )
-    })
-}
+
+                    exec.start({ hijack: true, stdin: true }, (err, stream) => {
+                        if (err) {
+                            console.log("Error while starting exec", err);
+                            return;
+                        }
+
+                        // Step 1: Stream processing
+                        processStreamOutput(stream, ws);
+
+                        // Step 2: Stream writing
+                        ws.on("message", (data) => {
+                            if (data === "getPort") {
+                                container.inspect((err, data) => {
+                                    const port = data.NetworkSettings;
+                                    console.log(port);
+                                });
+                                return;
+                            }
+                            stream.write(data);
+                        });
+                    });
+                });
+            });
+        });
+    });
+};
 
 function processStreamOutput(stream, ws) {
     let nextDataType = null; // Stores the type of the next message
@@ -47,14 +75,13 @@ function processStreamOutput(stream, ws) {
     let buffer = Buffer.from("");
 
     function processStreamData(data) {
-        // This is a helper function to process incoming data chunks
-        if(data) {
+        if (data) {
             buffer = Buffer.concat([buffer, data]); // Concatenating the incoming data to the buffer
         }
 
-        if(!nextDataType) {
+        if (!nextDataType) {
             // If the next data type is not known, then we need to read the next 8 bytes to determine the type and length of the message
-            if(buffer.length >= 8) {
+            if (buffer.length >= 8) {
                 const header = bufferSlicer(8);
                 nextDataType = header.readUInt32BE(0); // The first 4 bytes represent the type of the message
                 nextDataLength = header.readUInt32BE(4); // The next 4 bytes represent the length of the message
@@ -62,7 +89,7 @@ function processStreamOutput(stream, ws) {
                 processStreamData(); // Recursively call the function to process the message
             }
         } else {
-            if(buffer.length >= nextDataLength) {
+            if (buffer.length >= nextDataLength) {
                 const content = bufferSlicer(nextDataLength); // Slice the buffer to get the message content
                 ws.send(content); // Send the message to the client
                 nextDataType = null; // Reset the type and length of the next message
@@ -75,10 +102,9 @@ function processStreamOutput(stream, ws) {
     function bufferSlicer(end) {
         // this function slices the buffer and returns the sliced buffer and the remaining buffer
         const output = buffer.slice(0, end); // header of the chunk
-        buffer = Buffer.from(buffer.slice(end, buffer.length)); // remaining part of the chubk
+        buffer = Buffer.from(buffer.slice(end, buffer.length)); // remaining part of the chunk
 
         return output;
-
     }
 
     stream.on("data", processStreamData);
